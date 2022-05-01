9f311c45f854ca848e10bffbe7d3765110320b80
PaolaDaft <andrepaolagd@gmail.com>
2022-04-30 20:48:25 -0500

    Creación de una clase y modularización

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const MissionCommander = require('./app/missionCommander');
 const carlo = new MissionCommander("Carlo")
+const fer = new MissionCommander("Fernanda")
+const rodrigo = new MissionCommander("Rodrigo")
 console.log(carlo.name)
+console.log(fer.name)
+console.log(rodrigo.name)
